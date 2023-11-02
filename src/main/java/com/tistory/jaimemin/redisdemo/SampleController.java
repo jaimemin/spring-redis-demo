cf262130dc5f25e3d63f5dfb138cd5faa10c28a1
jaimemin <jaimemin@gmail.com>
2023-11-02 16:33:49 +0900

    Initial commit

---
+++ src/main/java/com/tistory/jaimemin/redisdemo/SampleController.java
@@ -0,0 +1,30 @@
+package com.tistory.jaimemin.redisdemo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/fruit")
+public class SampleController {
+
+    private final StringRedisTemplate redisTemplate;
+
+    @PostMapping
+    public String setFruit(@RequestParam String name) {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        ops.set("fruit", name);
+
+        return "saved.";
+    }
+
+    @GetMapping
+    public String getFruit() {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        String fruitName = ops.get("fruit");
+
+        return fruitName;
+    }
+}
