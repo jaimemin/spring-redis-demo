4252e822bad9bec46c35363a97e29e66b4aa5f49
jaimemin <jaimemin@gmail.com>
2023-11-05 23:48:52 +0900

    redis 설정을 통해 세션 클러스터링 적용

---
+++ src/main/java/com/tistory/jaimemin/redisdemo/LoginController.java
@@ -0,0 +1,27 @@
+package com.tistory.jaimemin.redisdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class LoginController {
+
+    private static final String KEY = "name";
+
+    @GetMapping("/login")
+    public String login(HttpSession session, @RequestParam String name) {
+        session.setAttribute(KEY, name);
+
+        return "login success.";
+    }
+
+    @GetMapping("/my-name")
+    public String myName(HttpSession session) {
+        String myName = (String) session.getAttribute(KEY);
+
+        return myName;
+    }
+}
